be5cd1cf46ec11cffa7a17bbadda1b910b4ad439
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-11-13 23:35:17 +0600

    from-table-complate

--- src/components/A_table.vue
+++ src/components/A_table.vue
@@ -1,49 +1,42 @@
 <template>
-     <table>
-          <tr>
-              <th>Id</th>
-              <th>Name</th>
-              <th>Brand</th>
-              <th>Catagory</th>
-              <th>Price</th>
-              <th>Quantity</th>
-              <th>Total</th>
-              <th>..</th>
-          </tr>
-          <tr v-for="(product, index) in products" :key="index">
-              <td> {{product.id}} </td>
-              <td>{{product.name}}</td>
-              <td>{{product.brand}}</td>
-              <td>{{product.catagory}}</td>
-              <td> {{product.price}}</td>
-              <td>{{product.quantity}}</td>
-              <td>{{product.total}}</td>
-              <td>
-                  <button @click="puja(index)" class="delete">Delete</button>
-                  <button class="edit">Edit</button>
-              </td>
-          </tr>
-  
-      </table>
-      <h1>Average : {{ grentTotal }}</h1>
+  <table>
+    <tr>
+      <th>Id</th>
+      <th>Name</th>
+      <th>Brand</th>
+      <th>Catagory</th>
+      <th>Price</th>
+      <th>Quantity</th>
+      <th>Total</th>
+      <th>..</th>
+    </tr>
+    <tr v-for="(product, index) in products" :key="index">
+      <td>{{ product.id }}</td>
+      <td>{{ product.name }}</td>
+      <td>{{ product.brand }}</td>
+      <td>{{ product.catagory }}</td>
+      <td>{{ product.price }}</td>
+      <td>{{ product.quantity }}</td>
+      <td>{{ product.total }}</td>
+      <td>
+        <button @click="puja(index)" class="delete">Delete</button>
+        <button class="edit">Edit</button>
+      </td>
+    </tr>
+  </table>
+  <h1>Average : {{ grentTotal }}</h1>
 </template>
 
 <script>
-    export default {
-        name:"A_table",
-        props:[
-            'products',
-            'grentTotal'
-        ],
-        methods:{
-        puja(index){
-                this.$emit("deleteProductItem",index, "hello")
-            
-          }
-        }
-    }
+export default {
+  name: "A_table",
+  props: ["products", "grentTotal"],
+  methods: {
+    puja(index) {
+      this.$emit("deleteProductItem", index, "hello");
+    },
+  },
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
